df5cc84caa58b2f6ba9cdee1b00d8a04a716836f
Jason Dillon <jason@planet57.com>
2015-01-21 20:21:01 -0800

    Extract helper to setup prototype security shits

---
+++ components/nexus-repository/src/main/java/org/sonatype/nexus/repository/security/RepositoryFormatSecurityResource.java
@@ -0,0 +1,89 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-2015 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+
+package org.sonatype.nexus.repository.security;
+
+import org.sonatype.nexus.repository.Format;
+import org.sonatype.security.model.SecurityModelConfiguration;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static org.sonatype.nexus.repository.security.BreadActions.ADD;
+import static org.sonatype.nexus.repository.security.BreadActions.BROWSE;
+import static org.sonatype.nexus.repository.security.BreadActions.DELETE;
+import static org.sonatype.nexus.repository.security.BreadActions.EDIT;
+import static org.sonatype.nexus.repository.security.BreadActions.READ;
+import static org.sonatype.nexus.repository.security.RepositoryFormatPrivilegeDescriptor.id;
+import static org.sonatype.nexus.repository.security.RepositoryFormatPrivilegeDescriptor.privilege;
+
+/**
+ * Repository format security resource.
+ *
+ * @since 3.0
+ */
+public class RepositoryFormatSecurityResource
+    extends MutableDynamicSecurityResource
+{
+  // NOTE: This is not ideal, but moving forward to allow sharing, refactor eventually
+
+  private final Format format;
+
+  public RepositoryFormatSecurityResource(final Format format) {
+    this.format = checkNotNull(format);
+
+    // apply initial configuration
+    apply(new Mutator()
+    {
+      @Override
+      public void apply(final SecurityModelConfiguration model) {
+        initial(model);
+      }
+    });
+  }
+
+  private void initial(final SecurityModelConfiguration model) {
+    String formatName = format.getValue();
+
+    // add repository-format privileges
+    model.addPrivilege(privilege(formatName, BROWSE));
+    model.addPrivilege(privilege(formatName, READ));
+    model.addPrivilege(privilege(formatName, EDIT));
+    model.addPrivilege(privilege(formatName, ADD));
+    model.addPrivilege(privilege(formatName, DELETE));
+
+    // add repository-format 'admin' role
+    model.addRole(new CRoleBuilder()
+        .id(String.format("%s-%s-admin", RepositoryFormatPrivilegeDescriptor.TYPE, formatName))
+        .privilege(id(formatName, BROWSE))
+        .privilege(id(formatName, READ))
+        .privilege(id(formatName, EDIT))
+        .privilege(id(formatName, ADD))
+        .privilege(id(formatName, DELETE))
+        .create());
+
+    // add repository-format 'readonly' role
+    model.addRole(new CRoleBuilder()
+        .id(String.format("%s-%s-readonly", RepositoryFormatPrivilegeDescriptor.TYPE, formatName))
+        .privilege(id(formatName, BROWSE))
+        .privilege(id(formatName, READ))
+        .create());
+
+    // add repository-format 'deployer' role
+    model.addRole(new CRoleBuilder()
+        .id(String.format("%s-%s-deployer", RepositoryFormatPrivilegeDescriptor.TYPE, formatName))
+        .privilege(id(formatName, BROWSE))
+        .privilege(id(formatName, READ))
+        .privilege(id(formatName, EDIT))
+        .privilege(id(formatName, ADD))
+        .create());
+  }
+}
--- plugins/basic/nexus-repository-simple/src/main/java/org/sonatype/nexus/repository/simple/internal/SimpleFormatSecurityResource.java
+++ plugins/basic/nexus-repository-simple/src/main/java/org/sonatype/nexus/repository/simple/internal/SimpleFormatSecurityResource.java
@@ -18,19 +18,7 @@ import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.sonatype.nexus.repository.Format;
-import org.sonatype.nexus.repository.security.CRoleBuilder;
-import org.sonatype.nexus.repository.security.MutableDynamicSecurityResource;
-import org.sonatype.nexus.repository.security.RepositoryFormatPrivilegeDescriptor;
-import org.sonatype.security.model.SecurityModelConfiguration;
-
-import static com.google.common.base.Preconditions.checkNotNull;
-import static org.sonatype.nexus.repository.security.BreadActions.ADD;
-import static org.sonatype.nexus.repository.security.BreadActions.BROWSE;
-import static org.sonatype.nexus.repository.security.BreadActions.DELETE;
-import static org.sonatype.nexus.repository.security.BreadActions.EDIT;
-import static org.sonatype.nexus.repository.security.BreadActions.READ;
-import static org.sonatype.nexus.repository.security.RepositoryFormatPrivilegeDescriptor.id;
-import static org.sonatype.nexus.repository.security.RepositoryFormatPrivilegeDescriptor.privilege;
+import org.sonatype.nexus.repository.security.RepositoryFormatSecurityResource;
 
 /**
  * Simple format security resource.
@@ -40,58 +28,10 @@ import static org.sonatype.nexus.repository.security.RepositoryFormatPrivilegeDe
 @Named
 @Singleton
 public class SimpleFormatSecurityResource
-    extends MutableDynamicSecurityResource
+    extends RepositoryFormatSecurityResource
 {
-  private final Format format;
-
   @Inject
   public SimpleFormatSecurityResource(final @Named(SimpleFormat.NAME) Format format) {
-    this.format = checkNotNull(format);
-
-    // apply initial configuration
-    apply(new Mutator()
-    {
-      @Override
-      public void apply(final SecurityModelConfiguration model) {
-        initial(model);
-      }
-    });
-  }
-
-  private void initial(final SecurityModelConfiguration model) {
-    String formatName = format.getValue();
-
-    // add repository-format privileges
-    model.addPrivilege(privilege(formatName, BROWSE));
-    model.addPrivilege(privilege(formatName, READ));
-    model.addPrivilege(privilege(formatName, EDIT));
-    model.addPrivilege(privilege(formatName, ADD));
-    model.addPrivilege(privilege(formatName, DELETE));
-
-    // add repository-format 'admin' role
-    model.addRole(new CRoleBuilder()
-        .id(String.format("%s-%s-admin", RepositoryFormatPrivilegeDescriptor.TYPE, formatName))
-        .privilege(id(formatName, BROWSE))
-        .privilege(id(formatName, READ))
-        .privilege(id(formatName, EDIT))
-        .privilege(id(formatName, ADD))
-        .privilege(id(formatName, DELETE))
-        .create());
-
-    // add repository-format 'readonly' role
-    model.addRole(new CRoleBuilder()
-        .id(String.format("%s-%s-readonly", RepositoryFormatPrivilegeDescriptor.TYPE, formatName))
-        .privilege(id(formatName, BROWSE))
-        .privilege(id(formatName, READ))
-        .create());
-
-    // add repository-format 'deployer' role
-    model.addRole(new CRoleBuilder()
-        .id(String.format("%s-%s-deployer", RepositoryFormatPrivilegeDescriptor.TYPE, formatName))
-        .privilege(id(formatName, BROWSE))
-        .privilege(id(formatName, READ))
-        .privilege(id(formatName, EDIT))
-        .privilege(id(formatName, ADD))
-        .create());
+    super(format);
   }
 }
