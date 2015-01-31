612ff4412913c10bbad4e1e6b81e902ea954e5b5
Jason Dillon <jason@planet57.com>
2015-01-31 14:26:25 -0800

    Add dummy mode to authshard

---
+++ testsuite/legacy-testsuite/src/test/java/org/sonatype/nexus/testsuite/DummyIT.java
@@ -0,0 +1,17 @@
+package org.sonatype.nexus.testsuite;
+
+import org.sonatype.sisu.litmus.testsupport.TestSupport;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Dummy integration-test for 'dummy' auto-sharding mode.
+ */
+@Ignore
+public class DummyIT
+  extends TestSupport
+{
+  @Test
+  public void ignore() {}
+}
