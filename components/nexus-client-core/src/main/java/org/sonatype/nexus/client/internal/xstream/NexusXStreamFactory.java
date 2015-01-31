6ac667a5f663227152be5d5d3cd0c026b7623197
Jason Dillon <jason@planet57.com>
2015-01-31 11:54:52 -0800

    adjust packages

--- components/nexus-client-core/src/main/java/org/sonatype/nexus/client/internal/xstream/NexusXStreamFactory.java
+++ components/nexus-client-core/src/main/java/org/sonatype/nexus/client/internal/xstream/NexusXStreamFactory.java
@@ -10,9 +10,10 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-package org.sonatype.nexus.client.internal;
+package org.sonatype.nexus.client.internal.xstream;
 
-import org.sonatype.nexus.client.internal.xstream.LookAheadXppDriver;
+import org.sonatype.nexus.client.internal.ErrorMessage;
+import org.sonatype.nexus.client.internal.ErrorResponse;
 import org.sonatype.nexus.rest.model.XStreamConfiguratorLightweight;
 
 import com.thoughtworks.xstream.XStream;
