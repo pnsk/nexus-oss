1475111be718acbfae2986b1e501af44ee79ece8
Jason Dillon <jason@planet57.com>
2015-02-01 13:32:46 -0800

    simplify configurators

--- plugins/restlet1x/nexus-restlet1x-model/src/main/java/org/sonatype/nexus/restlet1x/model/XStreamConfigurator.java
+++ plugins/restlet1x/nexus-restlet1x-model/src/main/java/org/sonatype/nexus/restlet1x/model/XStreamConfigurator.java
@@ -18,16 +18,36 @@ import org.sonatype.plexus.rest.resource.error.ErrorResponse;
 import com.thoughtworks.xstream.XStream;
 
 /**
- * This is the "old" XStream configurator, and is used on server side, in Nexus core. It's main problem is that it
- * assumes plexus-restlet-bridge is on classpath (untrue in case of clients, at least it's not wanted, as it has huge
- * dependency trail), as it relies on "error message" classes defined over there. If you are on client side, you don't
- * want to use this configurator, use the{@link XStreamConfiguratorLightweight} instead.
- *
- * @author cstamas
+ * XStream configurator.
  */
 public class XStreamConfigurator
 {
   public static XStream configureXStream(XStream xstream) {
-    return XStreamConfiguratorLightweight.configureXStream(xstream, ErrorResponse.class, ErrorMessage.class);
+    return configureXStream(xstream, ErrorResponse.class, ErrorMessage.class);
+  }
+
+  public static XStream configureXStream(XStream xstream, Class<?> errorResponseClazz, Class<?> errorMessageClazz) {
+    // protect against XSS, escape HTML from input.
+    xstream.registerConverter(new HtmlEscapeStringConverter());
+
+    xstream.processAnnotations(StatusResourceResponse.class);
+
+    xstream.alias("nexus-error", errorResponseClazz);
+    xstream.alias("error", errorMessageClazz);
+    xstream.registerLocalConverter(errorResponseClazz, "errors", new AliasingListConverter(errorMessageClazz, "error"));
+
+    xstream.alias("authentication-login", AuthenticationLoginResourceResponse.class);
+
+    xstream.registerLocalConverter(AuthenticationClientPermissions.class, "permissions",
+        new AliasingListConverter(ClientPermission.class, "permission"));
+
+    xstream.registerLocalConverter(StatusConfigurationValidationResponse.class, "validationErrors",
+        new AliasingListConverter(String.class, "error"));
+    xstream.registerLocalConverter(StatusConfigurationValidationResponse.class, "validationWarnings",
+        new AliasingListConverter(String.class, "warning"));
+
+    xstream.processAnnotations(AuthenticationLoginResourceResponse.class);
+
+    return xstream;
   }
 }
